da0dd698dfe6d8e523800d081aa629b1f2fb5164
Ritesh Pandey <rp140578@gmail.com>
2025-03-02 23:22:50 +0545

    chore(cursor): added route to home from logo

--- src/components/ui/logo-rotating.tsx
+++ src/components/ui/logo-rotating.tsx
@@ -1,14 +1,18 @@
 "use client"
 
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 export default function LogoRotating(){
+  const router= useRouter()
     return(
-        <div className="absolute top-0  transform left-10 -translate-x-1/2">
+        <div
+        onClick={()=>router.replace("/")}
+        className="absolute top-0 transform left-10 -translate-x-1/2">
         <Image
-          alt="R P"
+          alt="R P" 
           width="80"
           height="80"
-          className="animate-[spinX_3s_linear_infinite]"
+          className="animate-[spinX_3s_linear_infinite] "
           src="/images/main-logo-white.png"
         />
       </div>
