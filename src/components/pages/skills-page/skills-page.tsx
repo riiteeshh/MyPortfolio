8215af8c4e1752090211f9f324c287b019ff1807
Ritesh Pandey <rp140578@gmail.com>
2025-03-08 21:46:45 +0545

    style(skill-page): enhanced skills page

--- src/components/pages/skills-page/skills-page.tsx
+++ src/components/pages/skills-page/skills-page.tsx
@@ -2,62 +2,50 @@ import Image from "next/image";
 
 export default function SkillPage() {
   const skills = [
-    { src: "/images/css-3.svg", name: "CSS" },
+    { src: "/images/flutter.svg", name: "Flutter" },
+    { src: "/images/nextjs.svg", name: "Next.js" },
     { src: "/images/html-5.svg", name: "HTML" },
+    { src: "/images/css-3.svg", name: "CSS" },
     { src: "/images/js.svg", name: "JavaScript" },
     { src: "/images/dart.svg", name: "Dart" },
-  ];
-
-  const frameworks = [
-    { src: "/images/flutter.svg", name: "Flutter" },
+    { src: "/images/python.svg", name: "Python" },
+    { src: "/images/c.svg", name: "C" },
+    { src: "/images/c++.svg", name: "C++" },
     { src: "/images/tailwind.svg", name: "Tailwind CSS" },
-    { src: "/images/nextjs.svg", name: "Next.js" },
+    { src: "/images/bloc.svg", name: "Bloc" },
+    { src: "/images/git.svg", name: "Git" },
+    { src: "/images/github.svg", name: "Github" },
+    { src: "/images/android-studio.svg", name: "Android Studio" },
+    { src: "/images/docker.svg", name: "Docker" },
+    { src: "/images/firebase.svg", name: "Firebase" },
+    { src: "/images/jira.svg", name: "Jira" },
+    { src: "/images/figma.svg", name: "Figma" },
+    { src: "/images/confluence.svg", name: "Confluence" },
+    { src: "/images/linux.svg", name: "Linux" },
+    { src: "/images/vs-code.svg", name: "VS Code" },
   ];
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-6">
-              <h1 className="text-4xl md:text-5xl font-bold text-center mb-4">SKILLS</h1>
+      <h1 className="text-3xl md:text-3xl font-extrabold  text-center mb-4">
+        SKILLS
+      </h1>
 
-      <div className="flex gap-20 mt-10">
+      <div className="flex gap-10 mt-10 max-w-5xl flex-wrap items-center justify-center">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="flex flex-col items-center hover:scale-125 hover:text-4xl cursor-pointer"
+            className="flex flex-col items-center  cursor-pointer motion-safe:hover:scale-125 motion-safe:hover:text-4xl transition-all"
           >
             <Image
-              width={60}
-              height={60}
+              width={80}
+              height={80}
               alt={skill.name}
               className="transition-transform"
               src={skill.src}
             />
             <p className="mt-2 text-sm font-bold transition-all">
               {skill.name}
-            </p>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h2 className="mt-20 text-3xl font-bold text-center text-white italic">
-          Frameworks
-        </h2>
-        <hr className="h-0.5 w-full bg-white mt-2 px-2 pt-0" />
-      </div>
-      <div className="flex gap-20 mt-10">
-        {frameworks.map((framework, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center hover:scale-125 hover:text-4xl cursor-pointer"
-          >
-            <Image
-              width={60}
-              height={60}
-              alt={framework.name}
-              className=" transition-transform"
-              src={framework.src}
-            />
-            <p className="mt-2 text-sm font-bold transition-all">
-              {framework.name}
             </p>
           </div>
         ))}
